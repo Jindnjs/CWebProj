3e25a34c4c49a946d4e2090d85a0b6fab21ef376
Jindnjs <145753929+Jindnjs@users.noreply.github.com>
2024-07-10 18:26:09 +0900

    c1
    before add asset

---
+++ ch_web/src/main/java/com/site/ch_web/ChWebApplication.java
@@ -0,0 +1,13 @@
+package com.site.ch_web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChWebApplication.class, args);
+	}
+
+}
---
+++ ch_web/src/main/java/com/site/ch_web/MainController.java
@@ -0,0 +1,12 @@
+package com.site.ch_web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+	@GetMapping("/")
+	public String index() {
+		return "index";
+	}
+}
