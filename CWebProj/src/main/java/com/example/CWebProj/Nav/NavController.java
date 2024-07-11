40377da9fc67cf13c7d495bab13f55a705a5b5c1
Jindnjs <145753929+Jindnjs@users.noreply.github.com>
2024-07-12 00:26:11 +0900

    v1

---
+++ CWebProj/src/main/java/com/example/CWebProj/Nav/NavController.java
@@ -0,0 +1,13 @@
+package com.example.CWebProj.Nav;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class NavController {
+
+	@GetMapping("/nav/test")
+	public String navtest() {
+		return "/nav/nav";
+	}
+}
