2cb39081ec420d26ca7a6f0bce2d6ae9da46c819
JUN HYUK KWON <sojourn8th@naver.com>
2024-07-11 21:37:24 +0900

    회원가입 및 로그인.ver1

---
+++ CWebProj/src/main/java/com/example/CWebProj/Autho/AuthoController.java
@@ -0,0 +1,34 @@
+package com.example.CWebProj.Autho;
+
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+@Controller
+public class AuthoController {
+	@GetMapping("autho/")
+	public String index() {
+		
+		return "autho/index";
+	}
+	
+	@PreAuthorize("hasAuthority('ROLE_ADMIN')")
+	@GetMapping("/admin")
+	public String admin() {
+		
+		return "autho/admin";
+	}
+	
+	@PreAuthorize("hasAnyAuthority('ROLE_ADMIN','ROLE_MANAGER')")
+	@GetMapping("/manager")
+	public String manager() {
+		
+		return "autho/manager";
+	}
+	
+	@PreAuthorize("isAuthenticated()")
+	@GetMapping("/user")
+	public String user() {
+		
+		return "autho/user";
+	}
+}
