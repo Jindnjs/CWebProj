1227e04ceded737694643b014115dc6eed61ff35
Jindnjs <145753929+Jindnjs@users.noreply.github.com>
2024-07-12 19:13:56 +0900

    디비 변경작업

--- CWebProj/src/main/java/com/example/CWebProj/IndexController.java
+++ CWebProj/src/main/java/com/example/CWebProj/IndexController.java
@@ -6,12 +6,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class IndexController {
 
-	@GetMapping("/")
+	@GetMapping("/test")
 	public String test() {
 		return "test";
 	}
 	
-	@GetMapping("/main")
+	@GetMapping("/")
 	public String index() {
 		return "index";
 	}
