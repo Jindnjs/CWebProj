8dc5a57a859e28c565a5dc064603e5e150e4b5f9
Jindnjs <145753929+Jindnjs@users.noreply.github.com>
2024-07-16 00:50:47 +0900

    v1 메인화면 네비에 디비에서 데이터 출력

--- CWebProj/src/main/java/com/example/CWebProj/IndexController.java
+++ CWebProj/src/main/java/com/example/CWebProj/IndexController.java
@@ -1,5 +1,9 @@
 package com.example.CWebProj;
 
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
@@ -7,12 +11,17 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import com.example.CWebProj.Banner.BannerService;
+import com.example.CWebProj.Nav.MenuCateg;
+import com.example.CWebProj.Nav.NavService;
 
 @Controller
 public class IndexController {
 	
 	@Autowired
 	BannerService bannerService;
+	
+	@Autowired
+	NavService navService;
 	
 	@Value("${cloud.aws.s3.endpoint}")
 	private String downpath;
@@ -21,6 +30,10 @@ public class IndexController {
 	public String index(Model model) {
 		model.addAttribute("banners", bannerService.readlist());
         model.addAttribute("downpath", "https://" + downpath);
+        List<MenuCateg> menuCategories = navService.getAllMenuCategories();
+        Map<Integer, List<MenuCateg>> groupedMenuCategories = menuCategories.stream()
+                .collect(Collectors.groupingBy(MenuCateg::getMenuRate));
+        model.addAttribute("groupedMenuCategories", groupedMenuCategories);
 		return "index";
 	}
 	
