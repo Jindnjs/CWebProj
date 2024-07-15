2c30ce6c792d788565f03ea028be9170cef33673
Jindnjs <145753929+Jindnjs@users.noreply.github.com>
2024-07-15 20:35:08 +0900

    네비 수정 기능 v1

--- CWebProj/src/main/java/com/example/CWebProj/Nav/NavRepository.java
+++ CWebProj/src/main/java/com/example/CWebProj/Nav/NavRepository.java
@@ -3,9 +3,7 @@ package com.example.CWebProj.Nav;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface NavRepository extends JpaRepository<MenuCateg, Integer> {
 	
 	List<MenuCateg> findAllByOrderByMenuRateAscCategoryRateAsc();
