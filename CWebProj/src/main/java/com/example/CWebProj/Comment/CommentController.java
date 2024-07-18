c77f0b0169d7a75f1fe5ed9e5102aef3064f0a46
Jihwan-P <wlghks9619@gmail.com>
2024-07-19 01:38:53 +0900

    v1
    update, delete

--- CWebProj/src/main/java/com/example/CWebProj/Comment/CommentController.java
+++ CWebProj/src/main/java/com/example/CWebProj/Comment/CommentController.java
@@ -1,6 +1,7 @@
 package com.example.CWebProj.Comment;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -33,5 +34,31 @@ public class CommentController {
 		this.commentService.create(board, content);
 		return "redirect:" + menucateg.getBoardLink() + "/" + menuId + "/detail/" + boardId;
 	}
+
+	@GetMapping("/{menuId}/update/{boardId}/{commentId}")
+	public String updateComment(@PathVariable("commentId") Integer commentId) {
+		return "";
+	}
+	
+	@PostMapping("/update/{menuId}")
+	public String updateComment(@PathVariable("menuId") Integer menuId,
+								@RequestParam("commentId") Integer commentId,
+								@RequestParam("commentContent") String commentContent) {
+		MenuCateg menucateg = this.navService.getMenu(menuId);
+		Comment comment = this.commentService.getComment(commentId);
+		comment.setContent(commentContent);
+		this.commentService.update(comment);
+		return "redirect:" + menucateg.getBoardLink() + "/" + menuId + "/detail/" + commentService.getComment(commentId).getBoard().getId();
+	}
+	
+	@GetMapping("/{menuId}/delete/{boardId}/{commentId}")
+	public String commentDelete(@PathVariable("menuId") Integer menuId,
+								@PathVariable("boardId") Integer boardId,
+								@PathVariable("commentId") Integer commentId ) {
+		MenuCateg menucateg = this.navService.getMenu(menuId);
+		this.commentService.delete(commentId);
+		return "redirect:" + menucateg.getBoardLink() + "/" + menuId + "/detail/" + boardId;
+	}
+	
 	
 }
