fae9aa96e7692c0941b2bd3a50bb93249d350638
JUN HYUK KWON <sojourn8th@naver.com>
2024-08-08 17:35:58 +0900

    AutoList

---
+++ CWebProj/src/main/java/com/example/CWebProj/AutoList/AutoListRepository.java
@@ -0,0 +1,8 @@
+package com.example.CWebProj.AutoList;
+
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AutoListRepository extends JpaRepository<AutoList, Integer> {
+    List<AutoList> findByBoardId(Integer boardId);
+}
