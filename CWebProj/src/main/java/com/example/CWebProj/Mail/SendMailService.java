bbdc5a6146c34173665f61717544d05883546d29
Jindnjs <145753929+Jindnjs@users.noreply.github.com>
2024-09-02 14:06:33 +0900

    오류수정

--- CWebProj/src/main/java/com/example/CWebProj/Mail/SendMailService.java
+++ CWebProj/src/main/java/com/example/CWebProj/Mail/SendMailService.java
@@ -65,8 +65,8 @@ public class SendMailService {
 		
 		String content="송신 이메일 주소 : "+email+"<br>"+"내용 : "+message;
 		helper.setFrom("areumdownchurch1@gmail.com");
-		helper.setTo(email);//목사님 이메일로 
-		helper.setSubject(name+"님께서 매일을 보냈습니다   "+"제목 : "+subject); //메일 제목
+		helper.setTo("yoojinwon@naver.com");//목사님 이메일로 
+		helper.setSubject(name+"님께서 메일을 보냈습니다  "+"제목 : "+subject); //메일 제목
 		helper.setText(content, true);//메일 내용 
 		
 		mailSender.send(contact);
